b60f8a229c5f25cd44059bef5642ca6389d08bf3
ikamindeguia24 <ikamindeguia24@lhusurbil.eus>
2025-10-23 10:58:46 +0200

    bases de get, modificar y eliminar empezadas

--- api.py
+++ api.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+clientes = list()
 
-@app.route('/')
-def helloworld():
-    return 'Hello World'
+@app.route('/getDatos', methods=['GET'])
+def getDatos():
+    nombre = request.args.get('nombre')
+
+@app.route('/modificarCliente', methods=[''])
+
+@app.route('/eliminarCliente', methods=['DELETE'])
+def eliminarCliente(nombre):
+    for cliente in clientes:
+        if cliente[0].strip() == nombre.strip():
+            clientes.remove(cliente)
+            return jsonify({'mensaje' : f'Se ha eliminado a {nombre}'})
+        return jsonify({'mensaje' : 'No se ha eliminado a nadie'})
+
 
 if __name__ == '__main__':
     app.run()
+    #app.run(debug=True, port=8069,host='54.196.237.226')
