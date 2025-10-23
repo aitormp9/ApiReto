d9a64f7a35eeb21e2c4f892030db04580bdca411
ikmsuarez23 <ikmsuarez23@lhusurbil.eus>
2025-10-23 12:40:41 +0200

    metodo get hecho

--- api.py
+++ api.py
@@ -1,31 +1,37 @@
 from flask import Flask, request, jsonify
 import xmlrpc.client
 
+
 url = "http://reto1odoo.duckdns.org:8069"
-bd = "Reto1odoo"
+bd = "Reto1-TechSolutions"
 usuario = "mikelaitoribai"
 contrasena = "maireto"
 
 common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
 uid = common.authenticate(bd, usuario, contrasena, {})
 
+models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
+
 app = Flask(__name__)
-clientes = list()
+
 
 @app.route('/getDatos', methods=['GET'])
 def getDatos():
-    nombre = request.args.get('nombre')
+    nombre = request.args.get('nombre', '')
 
-@app.route('/modificarCliente', methods=[''])
+    domain = []
+    if nombre:
+    
+        domain = [['name', 'ilike', nombre]]
 
-@app.route('/eliminarCliente', methods=['DELETE'])
-def eliminarCliente(nombre):
-    for cliente in clientes:
-        if cliente[0].strip() == nombre.strip():
-            clientes.remove(cliente)
-            return jsonify({'mensaje' : f'Se ha eliminado a {nombre}'})
-        return jsonify({'mensaje' : 'No se ha eliminado a nadie'})
+    clientes = models.execute_kw(
+        bd, uid, contrasena,
+        'res.partner', 'search_read',
+        [domain],
+        {'fields': ['id', 'name', 'email']}
+    )
+    return jsonify(clientes)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
