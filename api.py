30946fea2a179e14bfa5599c474aa041233a8121
ikamindeguia24 <ikamindeguia24@lhusurbil.eus>
2025-10-24 09:28:33 +0200

    clientes añadidos

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ def getDatos():
     )
 
 
-    personas = r"C:\Users\ikmsuarez23\Desktop\Reto\ApiReto\usuario.json"
+    personas = r"C:\\aitormindeguia\\Reto1\\ApiReto1\\ApiReto\\usuario.json"
     with open(personas, "w", encoding="utf-8") as usuario:
         json.dump(clientes, usuario, indent=4, ensure_ascii=False)
 
